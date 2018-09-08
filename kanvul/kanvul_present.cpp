a94ed038adc224a824aa8aba3fff8cd6ecfe1ca9
Yuehang Wu <yuehan9.wu@gmail.com>
2018-09-08 22:55:53 +0800

    [KanVul] swapchain present extension

---
+++ kanvul/kanvul_present.cpp
@@ -0,0 +1,209 @@
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+class KanVul {
+public:
+    ~KanVul() {}
+    KanVul() {}
+    void InitWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        _glfw = glfwCreateWindow(800, 800, "KanVul_present", nullptr, nullptr);
+        //glfwShowWindow(_gflw);
+    }
+
+    void DestroyWindow() {
+        glfwDestroyWindow(_glfw);
+        glfwTerminate();
+    }
+
+    void InitWindowSurface() {
+        glfwCreateWindowSurface(_inst, _glfw, nullptr, &_surf);
+    }
+
+    void Run() {
+        while (!glfwWindowShouldClose(_glfw)) {
+            glfwPollEvents();
+        }
+    }
+
+    void InitVulkanCore() {
+        InitInstance();
+        InitWindowSurface();
+        InitPhysicalDevice();
+        InitDevice();
+        InitSwapchain();
+    }
+
+    void DestroyVulkanCore() {
+        for (const auto iter : _swpchain_imgv) {
+            vkDestroyImageView(_dev, iter, nullptr);
+        }
+        vkDestroySwapchainKHR(_dev, _swpchain, nullptr);
+        vkDestroyDevice(_dev, nullptr);
+        vkDestroySurfaceKHR(_inst, _surf, nullptr);
+        vkDestroyInstance(_inst, nullptr);
+    }
+
+    void InitInstance() {
+        VkApplicationInfo ai {};
+        ai.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        ai.pNext = nullptr;
+        ai.pApplicationName = "KanVul_present";
+        ai.applicationVersion = 0;
+        ai.pEngineName = "KanVul_present";
+        ai.engineVersion = 0;
+        ai.apiVersion = VK_API_VERSION_1_1;
+
+        VkInstanceCreateInfo ii {};
+        ii.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        ii.pNext = nullptr;
+        ii.pApplicationInfo = &ai;
+        ii.enabledLayerCount = 0;
+        ii.ppEnabledLayerNames = nullptr;
+
+        vector<const char *> ie;
+        ie.push_back("VK_KHR_surface");
+        ie.push_back("VK_KHR_xcb_surface");
+        ii.enabledExtensionCount = ie.size();
+        ii.ppEnabledExtensionNames = ie.data();
+
+        vkCreateInstance(&ii, nullptr, &_inst);
+    }
+
+    void InitPhysicalDevice() {
+        uint32_t physicalDeviceCount = 0;
+        vkEnumeratePhysicalDevices(_inst, &physicalDeviceCount, nullptr);
+        _pdev.resize(physicalDeviceCount);
+        vkEnumeratePhysicalDevices(_inst, &physicalDeviceCount, _pdev.data());
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(_pdev[0], &queueFamilyCount, nullptr);
+        std::vector<VkQueueFamilyProperties> queueFamily;
+        queueFamily.resize(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(_pdev[0], &queueFamilyCount, queueFamily.data());
+    }
+
+    void InitDevice() {
+        VkDeviceQueueCreateInfo qi {};
+        qi.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        qi.pNext = nullptr;
+        qi.flags = 0;
+        qi.queueFamilyIndex = 0;
+        qi.queueCount = 1;
+        float priority = 1.0;
+        qi.pQueuePriorities = &priority;
+
+        VkDeviceCreateInfo di;
+        di.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        di.pNext = nullptr;
+        di.flags = 0;
+        di.queueCreateInfoCount = 1;
+        di.pQueueCreateInfos = &qi;
+        di.enabledLayerCount = 0;
+        di.ppEnabledLayerNames = nullptr;
+
+        vector<const char *> de;
+        de.push_back("VK_KHR_swapchain");
+        di.enabledExtensionCount = de.size();
+        di.ppEnabledExtensionNames = de.data();
+        di.pEnabledFeatures = nullptr;
+
+        vkCreateDevice(_pdev[0], &di, nullptr, &_dev);
+        VkBool32 presentSupported = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(_pdev[0], 0, _surf, &presentSupported);
+        assert(VK_TRUE == presentSupported);
+    }
+
+    void InitSwapchain() {
+        uint32_t fmtc = 0;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(_pdev[0], _surf, &fmtc, nullptr);
+        std::vector<VkSurfaceFormatKHR> fmt;
+        fmt.resize(fmtc);
+        vkGetPhysicalDeviceSurfaceFormatsKHR(_pdev[0], _surf, &fmtc, fmt.data());
+
+        VkSurfaceCapabilitiesKHR cap;
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(_pdev[0], _surf, &cap);
+
+        VkSwapchainCreateInfoKHR sci;
+        sci.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+        sci.pNext = nullptr;
+        sci.flags = 0;
+        sci.surface = _surf;
+        sci.minImageCount = cap.minImageCount;
+        sci.imageFormat = fmt[0].format;
+        sci.imageColorSpace = fmt[0].colorSpace;
+        sci.imageExtent = cap.currentExtent;
+        sci.imageArrayLayers = 1;
+        sci.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+        sci.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        sci.queueFamilyIndexCount = 0;
+        sci.pQueueFamilyIndices = nullptr;
+        sci.preTransform = cap.currentTransform;
+        sci.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+        sci.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+        sci.clipped = VK_TRUE;
+        sci.oldSwapchain = VK_NULL_HANDLE;
+
+        vkCreateSwapchainKHR(_dev, &sci, nullptr, &_swpchain);
+
+        uint32_t scimgc = 0;
+        vkGetSwapchainImagesKHR(_dev, _swpchain, &scimgc, nullptr);
+        _swpchain_img.resize(scimgc);
+        _swpchain_imgv.resize(scimgc);
+        vkGetSwapchainImagesKHR(_dev, _swpchain, &scimgc, _swpchain_img.data());
+
+        for (uint32_t i = 0; i < scimgc; i++) {
+            VkImageViewCreateInfo imgvi;
+            imgvi.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            imgvi.pNext = nullptr;
+            imgvi.flags = 0;
+            imgvi.image = _swpchain_img[i];
+            imgvi.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            imgvi.format = fmt[0].format;
+            imgvi.components.r = VK_COMPONENT_SWIZZLE_R;
+            imgvi.components.g = VK_COMPONENT_SWIZZLE_G;
+            imgvi.components.b = VK_COMPONENT_SWIZZLE_B;
+            imgvi.components.a = VK_COMPONENT_SWIZZLE_A;
+            imgvi.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            imgvi.subresourceRange.baseMipLevel = 0;
+            imgvi.subresourceRange.levelCount = 1;
+            imgvi.subresourceRange.baseArrayLayer = 0;
+            imgvi.subresourceRange.layerCount = 1;
+
+            vkCreateImageView(_dev, &imgvi, nullptr, &_swpchain_imgv[i]);
+        }
+    }
+
+private:
+    GLFWwindow *_glfw;
+    /* vulkan core */
+    VkInstance _inst;
+    VkSurfaceKHR _surf;
+    vector<VkPhysicalDevice> _pdev;
+    VkDevice _dev;
+    VkSwapchainKHR _swpchain;
+    vector<VkImage> _swpchain_img;
+    vector<VkImageView> _swpchain_imgv;
+};
+
+int main(int argc, char const *argv[])
+{
+    KanVul app;
+    app.InitWindow();
+    app.InitVulkanCore();
+    app.DestroyVulkanCore();
+    app.DestroyWindow();
+
+    return 0;
+}
