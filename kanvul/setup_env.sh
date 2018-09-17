ad46f4a6f6cce7017b9efd373fc30d3ec5fc26f9
Yuehang Wu <yuehan9.wu@gmail.com>
2018-09-17 21:08:54 +0800

    [KanVul] vkCmdBindVertexBuffers
    demo single attribute vertex binding for vkCmdDraw

--- kanvul/setup_env.sh
+++ kanvul/setup_env.sh
@@ -3,4 +3,5 @@
 export VK_SDK_PATH=/home/vk/1.1.82.1/x86_64
 export LD_LIBRARY_PATH=$VK_SDK_PATH/lib
 export VK_LAYER_PATH=$VK_SDK_PATH/etc/explicit_layer.d
-export VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_standard_validation:VK_LAYER_LUNARG_api_dump
+export VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_standard_validation
+#export VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_standard_validation:VK_LAYER_LUNARG_api_dump
