f205432914d87bafaaa65333774c40cd22841593
Yuehang Wu <yuehan9.wu@gmail.com>
2018-10-26 22:39:14 +0800

    [Volcano] add mouse control object spinning

--- volcano/controller.hpp
+++ volcano/controller.hpp
@@ -2,7 +2,10 @@
 #define _CAMERA_HPP_
 
 #include <GLFW/glfw3.h>
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
 class CameraRoam {
@@ -111,6 +114,102 @@ public:
     glm::vec3 loc;
     glm::vec3 up;
     glm::vec3 front;
+    glm::mat4 view_mat;
+    glm::mat4 proj_mat;
+    glm::vec2 center;
+};
+
+class Spinner {
+public:
+    struct Posture {
+        float deltatime;
+        float lasttime;
+        float lastX;
+        float lastY;
+        float xmarch;
+        float ymarch;
+        bool valid;
+    };
+    ~Spinner() {}
+    Spinner() {}
+    Spinner(float w, float h) {
+        glm::vec3 loc = glm::vec3(0, 0, 5);
+        glm::vec3 front = glm::vec3(0, 0, -1);
+        glm::vec3 up = glm::vec3(0, 1, 0);
+        model_mat = glm::mat4(1.0);
+        view_mat = glm::lookAt(loc, loc + front, up);
+        proj_mat = glm::perspective(glm::radians(60.0f), 1.0f, 0.1f, 100.0f);
+
+        posture.deltatime = 0;
+        posture.lasttime = 0;
+        posture.lastX = w / 2;
+        posture.lastY = h / 2;
+        posture.xmarch = 0;
+        posture.ymarch = 0;
+        posture.valid = false;
+
+        center = glm::vec2(w/2, h/2);
+    }
+
+    void onKeyEvent(GLFWwindow* win, int key, int scancode, int action, int mode) {
+        if (key == GLFW_KEY_ESCAPE) {
+            glfwSetWindowShouldClose(win, GLFW_TRUE);
+        }
+    }
+
+    void onMouseEvent(GLFWwindow *win, double xpos, double ypos) {
+        if (posture.valid == true) {
+            float xoffset = xpos - posture.lastX;
+            float yoffset = ypos - posture.lastY;
+
+            posture.lastX = xpos;
+            posture.lastY = ypos;
+
+            float sensitivity = 0.15f;
+            xoffset *= sensitivity;
+            yoffset *= sensitivity;
+
+            posture.xmarch = xoffset;
+            posture.ymarch = yoffset;
+
+            glm::vec3 reference = glm::vec3(0, 0, 1);
+            glm::mat4 gx = glm::rotate(glm::radians(posture.xmarch), glm::vec3(0.0, 1.0, 0.0));
+            reference = glm::mat3(gx) * reference;
+            glm::vec3 axis = glm::cross(reference, glm::vec3(0, 1, 0));
+            glm::mat4 gy = glm::rotate(glm::radians(posture.ymarch), axis);
+            model_mat = gy * gx * model_mat;
+        }
+    }
+
+    void onMouseButtonEvent(GLFWwindow *win, int button, int action, int mode) {
+        if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+            double x, y;
+            glfwGetCursorPos(win, &x, &y);
+            posture.lastX = x;
+            posture.lastY = y;
+            posture.valid = true;
+        }
+
+        if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+            posture.valid = false;
+        }
+    }
+
+    inline void advance() {
+        float currenttime = glfwGetTime();
+        posture.deltatime = currenttime - posture.lasttime;
+        posture.lasttime = currenttime;
+    }
+
+    inline void compute() {}
+
+    inline glm::mat4 mvp() const {
+        return proj_mat * view_mat * model_mat;
+    }
+
+public:
+    struct Posture posture;
+    glm::mat4 model_mat;
     glm::mat4 view_mat;
     glm::mat4 proj_mat;
     glm::vec2 center;
